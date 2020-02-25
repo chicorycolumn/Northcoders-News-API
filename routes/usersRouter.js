35d759c060a8b50368fa244f68768f42e9e4be49
C <chrismatusemail@gmail.com>
2020-02-25 00:00:02 +0000

    ..

--- routes/usersRouter.js
+++ routes/usersRouter.js
@@ -2,6 +2,8 @@ const usersRouter = require('express').Router() // still using express, right?
 const { getUserByID } = require('../controllers/users.controller')
 const {handle405s} = require('../errors/errors')
 
-usersRouter.route('/:username').get(getUserByID).all(handle405s)
+usersRouter.route('/:username')
+    .get(getUserByID)
+    .all(handle405s)
 
 module.exports = usersRouter
